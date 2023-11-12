47d6ac5da5a84edc04fe2a465083b5ce24305d51
Petre Iordanescu <petre.iordanescu@gmail.com>
2023-11-13 01:10:07 +0200

    wip... 231113piu_a `rdinv.py` clean & clear code

--- xl2roefact/modules/rdinv.py
+++ xl2roefact/modules/rdinv.py
@@ -35,7 +35,7 @@ def rdinv(file_to_process: str, invoice_data_worksheet: str = None):
         - `invoice_data_worksheet`: the worksheet containing invoice
 
     Return:
-        - tuple of `(invoice_header_area: #FIXME_unknown_yet, invoice_lines_area: dict, invoice_footer_area: #FIXME_unknown_yet)`
+        - tuple of `(invoice_header_area: dict, invoice_lines_area: dict, invoice_footer_area: dict)`
 
     Important variables:
         - `db`: htxl object with invoice EXCEL (as a whole)
@@ -68,21 +68,22 @@ def rdinv(file_to_process: str, invoice_data_worksheet: str = None):
 
     # -----------------
     # detect all cells that should be changed to SYS_FILLED_EMPTY_CELL (these are cells id merged groups where first cell in merged group is relevant (diff from empty))
-    detected_cells_which_will_be_fake_filled = _get_merged_cells_tobe_changed(file_to_process, invoice_worksheet_name) # ...hereuare...
-    #print(f"----------DETECTED RANGES: {detected_cells_which_will_be_fake_filled}") #FIXME drop me as debugging
+    detected_cells_which_will_be_fake_filled = _get_merged_cells_tobe_changed(file_to_process, invoice_worksheet_name)
+    #print(f"----------DETECTED RANGES: {detected_cells_which_will_be_fake_filled}") #NOTE for debug purposes
     # scan all detectected cell and change them
     for _cell_index in detected_cells_which_will_be_fake_filled:
         _cell_row = _cell_index[0]
         _cell_col = _cell_index[1]
         ws.update_index(row = _cell_row, col = _cell_col, val = SYS_FILLED_EMPTY_CELL)
-        #print(f"cell {_cell_index} updated. new read va is: {ws.index(row = _cell_row, col = _cell_col)}") #FIXME drop after test
 
     ''' #NOTE quick plan:
+    - [x] variable names for zones: `invoice_header_area`, `invoice_lines_area`, `invoice_footer_area`
     - [x] detected `invoice_lines_area`
     - [ ] isolate header and footer after clean `invoice_lines_area`
-    - [x] variable names for zones: `invoice_header_area`, `invoice_lines_area`, `invoice_footer_area`
     '''
-    # -----------------
+    #
+    # #NOTE ---------[ work for `invoice_lines_area` ]---------
+    #
     # string-markers to search for to isolate `invoice_lines_area` #TODO to be unified (how? - search the cell containg text fragments --> get text from that cell --> use it as `ssd()` parameter)
     # #NOTE_ATT_README: tmp_items_table_marker_string = "crt" #FIXME drop me - DOES NOT WORK, search is exact... TRY THIS as "unified string"
     tmp_items_table_marker_string = "No. crt." #NOTE Kraftlangen invoice
@@ -97,75 +98,77 @@ def rdinv(file_to_process: str, invoice_data_worksheet: str = None):
     #TODO test if list has more items (ie, that means more item tables that will need to be consolidated)
     if isinstance(invoice_lines_area, list) and len(invoice_lines_area) > 0:
         #NOTE `invoice_lines_area` dictionary with keys: "keyrows", "keycols" and "data" (self explanatory)
-        invoice_lines_area = invoice_lines_area[0] #NOTE NOW will suppose is just one AND retain only first one (index [0]) - SEE AFTER TEST with RENware invoice...
+        invoice_lines_area = invoice_lines_area[0] #NOTE NOW will suppose found just one AND retain only first one (index [0]) - SEE AFTER TEST with RENware invoice...
 
-    # -----------------
     # clean cells that contains `SYS_FILLED_EMPTY_CELL`
-    for _i, _val in enumerate(invoice_lines_area["keycols"]): # loop for 'keycols' keyword
-        invoice_lines_area["keycols"][_i] = "" if _val == SYS_FILLED_EMPTY_CELL else invoice_lines_area["keycols"][_i]
-    for _i, _val in enumerate(invoice_lines_area["keyrows"]): # loop for 'keyrows' keyword
-        invoice_lines_area["keyrows"][_i] = "" if _val == SYS_FILLED_EMPTY_CELL else invoice_lines_area["keyrows"][_i]
+    invoice_lines_area["keycols"] = ["" if _i == SYS_FILLED_EMPTY_CELL else _i for _i in invoice_lines_area["keycols"]] # loop for 'keycols' keyword
+    invoice_lines_area["keyrows"] = ["" if _i == SYS_FILLED_EMPTY_CELL else _i for _i in invoice_lines_area["keyrows"]] # loop for 'keyrows' keyword
     for _tmp_row_index, _tmp_row in enumerate(invoice_lines_area["data"]): # # loop for 'data' keyword (first loop table rows, "data" key is matrix of lines & cells, ie as [][])
-        for _i, _val in enumerate(_tmp_row):
-            _tmp_row[_i] = "" if _val == SYS_FILLED_EMPTY_CELL else _tmp_row[_i]
+        _tmp_row = ["" if _i == SYS_FILLED_EMPTY_CELL else _i for _i in _tmp_row]
+        invoice_lines_area["data"][_tmp_row_index] = _tmp_row
 
     #TODO ...hereuare... @IMP==> need to clean empty rows
+
 
     #FIXME test area & notes starts here
     print(f"{Fore.YELLOW}---> TEST-note: tabel 0 linii factura contine:{Style.RESET_ALL}") #NOTE test should be an array of arrays (matrix) with invoice items #FIXME drop after test
     pprint(invoice_lines_area, width = 132) #FIXME drop after test
     print()
-
     '''#NOTE rezultat obtinut: "---> TEST-note: tabel 0 linii factura contine:...(nxt lines)..." #NOTE PASS OK for Kraftlangen invoice, only one line and data is OK
-{
-    'data': [
-        ['', '', '', '', '', '', ''],
-        ['Inlocuit conducta PSI coloana C2 DAV.', '', 1, 42756.08, 0.19, 42756.08, 8123.6552]
-    ],
-    'keycols': [
-        'Denumirea lucrarii                                  (Request description)',
-        '',
-        'Cantitatea (Quantity)',
-        'Pret unitar (Unit price)',
-        'Cota TVA (VAT %)',
-        'Valoare fara TVA (Value without VAT)',
-        'Valoare TVA (Value VAT)'
-    ],
-    'keyrows': [
-        '',
-        1
-    ]
- }
+        {
+            'data': [
+                ['', '', '', '', '', '', ''],
+                ['Inlocuit conducta PSI coloana C2 DAV.', '', 1, 42756.08, 0.19, 42756.08, 8123.6552]
+            ],
+            'keycols': [
+                'Denumirea lucrarii                                  (Request description)',
+                '',
+                'Cantitatea (Quantity)',
+                'Pret unitar (Unit price)',
+                'Cota TVA (VAT %)',
+                'Valoare fara TVA (Value without VAT)',
+                'Valoare TVA (Value VAT)'
+            ],
+            'keyrows': [
+                '',
+                1
+            ]
+        }
     '''
 
     '''#NOTE rezultat obtinut: "---> TEST-note: tabel 0 linii factura contine:...(nxt lines)..." #NOTE PASS OK for RENware invoice, only one line and data is OK
-{
-    'data': [
-        ['', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', 1, '', '', '', 2, 3, '', 4, '5 = 3 x 4', '6 = 5 x 19%', ''],
-        ['', 'Elaborare documentatie tehnica aplicatie NEXGEN.AI', '', '', '', 'buc', 1, '', 14807.4, 14807.4, 2813.41, '']
-    ],
-    'keycols': [
-        '',
-        'Denumirea produselor sau a serviciilor',
-        '',
-        '',
-        '',
-        'UM',
-        'Cant.',
-        '',
-        'Pret unitar\n(fara TVA)\n- RON -',
-        'Valoarea\n(fara TVA)\n- RON -',
-        'TVA\n- RON -',
-        ''
-    ],
-    'keyrows': [
-        '',
-        '0',
-        1
-    ]
- }
+        {
+            'data': [
+                ['', '', '', '', '', '', '', '', '', '', '', ''],
+                ['', 1, '', '', '', 2, 3, '', 4, '5 = 3 x 4', '6 = 5 x 19%', ''],
+                ['', 'Elaborare documentatie tehnica aplicatie NEXGEN.AI', '', '', '', 'buc', 1, '', 14807.4, 14807.4, 2813.41, '']
+            ],
+            'keycols': [
+                '',
+                'Denumirea produselor sau a serviciilor',
+                '',
+                '',
+                '',
+                'UM',
+                'Cant.',
+                '',
+                'Pret unitar\n(fara TVA)\n- RON -',
+                'Valoarea\n(fara TVA)\n- RON -',
+                'TVA\n- RON -',
+                ''
+            ],
+            'keyrows': [
+                '',
+                '0',
+                1
+            ]
+        }
     '''
+
+    #
+    # #NOTE END of ---------[ work for `invoice_lines_area` ]---------
+    #
+
 
 
     #NOTE result is: (`invoice_header_area`, `invoice_lines_area`, `invoice_footer_area`)
