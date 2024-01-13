62fdbba43c1de48c8724da6cb852f7f8d3e8caf2
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-01-13 10:03:17 +0200

    made plan ro continue with found info

--- xl2roefact/xl2roefact/rdinv.py
+++ xl2roefact/xl2roefact/rdinv.py
@@ -317,6 +317,18 @@ def rdinv(
     _temp_found_data={'value': 'Coralilor Nr. 22', 'location': (12, 2), 'label_value': 'Str. Coralilor Nr. 22', 'label_location': (12, 2)}
         * RENware invoice:
     _temp_found_data={'value': 'Bucureşti Sectorul 1, Strada BUZEŞTI, Nr. 71, Etaj 7 si 8', 'location': (12, 6), 'label_value': 'Adresa:', 'label_location': (12, 5)}
+    ---> CONTINUE plan:
+      1. chk if label val contains "adr" or "addr" -
+          _tmpstr = _temp_found_data['label_value'].lower())
+          _val_is_full_addr = ("adr" in _tmpstr) or ("addr" in _tmpstr)
+      2. test if is a full address -
+          if _val_is_full_addr:
+              ...  # have a full addr in xxx
+          else:
+              ...  # need re-search by individual address components... how make it ?
+              ...  # - one idea is to consider a long area in DOW with them...
+              ...  # - another idea is to try individual str like "str", "city", ...
+      3. anyway do NOT forget: COUNTRY is important & required
     '''
     ... #TODO ............hereuare............
     ... #FIXME opis `240113piu_a` effective code ENDS here
