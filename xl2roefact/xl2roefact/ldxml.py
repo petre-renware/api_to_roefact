b9129879283c87e1fb5c57943bfefd550ea98d11
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-02-10 08:08:10 +0200

    Update ldxml.py

--- xl2roefact/xl2roefact/ldxml.py
+++ xl2roefact/xl2roefact/ldxml.py
@@ -2,11 +2,13 @@
 """ ldxml: modul de incarcare a facturii in sistemul ANAF E-Factura
 
 Identification:
+
 * code-name: `ldxml`
 * copyright: (c) 2023 RENWare Software Systems
 * author: Petre Iordanescu (petre.iordanescu@gmail.com)
 
 Specifications:
+
 * document: `110-SRE-api_to_roefact_requirements.md` section `Componenta xl2roefact`
 * INTRARI: fisier `f-XML`
 * IESIRI: raport cu validarea si identificatorul incarcarii
