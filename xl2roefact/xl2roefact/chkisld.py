412e922c7c320dbe096df72faf743879d9c6c2e0
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-02-10 08:16:27 +0200

    Update chkisld.py

--- xl2roefact/xl2roefact/chkisld.py
+++ xl2roefact/xl2roefact/chkisld.py
@@ -9,7 +9,7 @@ Identification:
 
 Specifications:
 
-* document: `110-SRE-api_to_roefact_requirements.md` section `Componenta xl2roefact`
+* document cerinte initiale: `110-SRE-api_to_roefact_requirements.md` section `Componenta xl2roefact`
 * INTRARI: fisier `f-XLSX` sau numarul / cheia / codul facturii
 * IESIRI: valoarea echivalent `TRUE` daca factura a fost deja incarcata sau valoare echivalent `FALSE` daca factura nu a fost incarcata
 """
