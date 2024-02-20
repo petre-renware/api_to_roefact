67fe8181e8bc46bb5038970f7938cae5a6f4cfad
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-02-20 04:40:58 +0200

    Update rdinv.py

--- xl2roefact/xl2roefact/rdinv.py
+++ xl2roefact/xl2roefact/rdinv.py
@@ -901,14 +901,13 @@ def _build_meta_info_key(excel_file_to_process: str,
         ("cbc_PostalZone", "cbc:PostalZone"),  # invoice customer inforation - DETAIL L3 RECORD
         ("cac_Country", "cac:Country"),  # invoice customer inforation - DETAIL L3 RECORD
         ("cbc_IdentificationCode", "cbc:IdentificationCode"),  # invoice customer inforation - DETAIL L3 RECORD
-            #-----check.me----
-        ("LineVatAmount", None),  # line / item total VAT. Has no correspondent in XML schema
-        ("cac_Contact", "cac:Contact"),  #  customer contact information: email, phone
-        ("cbc_Telephone", "cbc:Telephone"),  # customer phone
-        ("cbc_ElectronicMail", "cbc:ElectronicMail"),  # customer email
-        ("RegCom", None),  # customer commerce register number (company legal registration number). Has no correspondent in XML schema
-        ("Bank", None),  # customer bank. Has no correspondent in XML schema
-        ("IBAN", None),  # customer bank account number (IBAN). Has no correspondent in XML schema
+        ("LineVatAmount", None),  # line / item total VAT. Has no correspondent in XML schema - DETAIL L3 RECORD
+        ("cac_Contact", "cac:Contact"),  #  customer contact information: email, phone - DETAIL L2 RECORD
+        ("cbc_Telephone", "cbc:Telephone"),  # customer phone -- DETAIL L3 RECORD
+        ("cbc_ElectronicMail", "cbc:ElectronicMail"),  # customer email - DETAIL L3 RECORD
+        ("RegCom", None),  # customer commerce register number (company legal registration number). Has no correspondent in XML schema - DETAIL L3 RECORD
+        ("Bank", None),  # customer bank. Has no correspondent in XML schema - DETAIL L3 RECORD
+        ("IBAN", None),  # customer bank account number (IBAN). Has no correspondent in XML schema - DETAIL L3 RECORD
         #TODO ...here to add items ref `cac_PostalAddress` - DETAIL L3 RECORDS
     ]
 
