26b5882c5ad3fc86d446ae225055baac4096609f
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-02-28 07:24:35 +0200

    root PDM environment, updated `requirements.txt` prepared to import

---
+++ docs/xl2roefact/tests/adhoc.cmd
@@ -0,0 +1,14 @@
+
+rem ---------------
+rem copyright (c) 2024 Petre Iordanescu, petre.iordanescu@yahoo.com, RENware Software Systems
+rem this command script will be run on CI automation workflow;
+rem     - workflow; `adhoc-run.yml`
+rem     - trigger event: merge to branch `adhoc`
+rem     - stdout redirection: `./tests/_test_results.txt`
+rem     - crt directory; `xl2roefact/`
+rem -------------------------
+
+echo "=== Test run of `adhoc.cmd` ======"
+
+
+
