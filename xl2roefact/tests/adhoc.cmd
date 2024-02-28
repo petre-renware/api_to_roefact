345ad3b3135476d347c68941e7ef14f92c00ec6f
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-02-28 05:52:13 +0200

    Update adhoc.cmd

--- xl2roefact/tests/adhoc.cmd
+++ xl2roefact/tests/adhoc.cmd
@@ -1,9 +1,9 @@
 
 rem ---------------
-rwm copyright (c) 2024 Petre Iordanescu, petre.iordanescu@yahoo.com, RENware Software Systems
+rem copyright (c) 2024 Petre Iordanescu, petre.iordanescu@yahoo.com, RENware Software Systems
 rem this command script will be run on CI automation workflow;
 rem     - workflow; `adhoc-run.yml`
-ren     - trigger event: merge to branch `adhoc`
+rem     - trigger event: merge to branch `adhoc`
 rem     - stdout redirection: `./tests/_test_results.txt`
 rem     - crt directory; `xl2roefact/`
 rem -------------------------
