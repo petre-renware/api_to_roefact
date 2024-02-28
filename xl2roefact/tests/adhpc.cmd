5e24abf1c47010bff8657c38eea9de28f0eecf37
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-02-28 05:11:52 +0200

    Create adhpc.cmd

---
+++ xl2roefact/tests/adhpc.cmd
@@ -0,0 +1,12 @@
+
+# this command script will be run on CI automation workflow;
+#     - workflow; `adhoc-run.yml`
+#     - trigger event: merge to branch `adhoc`
+#     - stdout & stderr redirection: `./tests/_test_results.txt`
+#     - crt directory; `xl2roefact/`
+
+
+echo "=== Test run of `adhoc.cmd` ======"
+
+
+
