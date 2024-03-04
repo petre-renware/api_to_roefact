878141be683d26cd9e68d36e0a0f9ab73c40cc73
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-03-04 16:36:30 +0200

    new comments re continue logic of `libutils`
    * Update libutils.py
    
    * Automated xl2roefact test-run (by RENware Software Systems)
    
    * Update libutils.py
    
    * Update libutils.py
    
    * Automated xl2roefact test-run (by RENware Software Systems)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- xl2roefact/xl2roefact/libutils.py
+++ xl2roefact/xl2roefact/libutils.py
@@ -77,24 +77,16 @@ def invoice_taxes_summary(
         # ... sum += (ExtAmnt ai VatAmnt) for this key
         # ... if exists should be updated.
         # ... if not take care to add with 0 as being first time when add it
-        ''' #FIXME dbg can drop. INFORMATIA CREATA pina in acest punct este: :
-        {
-            "cbc_LineExtensionAmount": 38890.25,
-            "LineVatAmount": 7389.15,
-            "cac_TaxCategory": {
-                "cac_ClassifiedTaxCategory": {
-                    "cbc_Percent": 0.19,
-                    "cac_TaxScheme": {
-                        "cbc_ID": "VAT"
-                    }
-                },
-                "ID": "S"
-            }
-        }
-        '''  # si de verificat nivelul "cac_Item" care trebuie pastrat doar continutul sau, ie work_cac_item["cac_Item"]
         req_item_info["cac_TaxCategory"]["ID"] = "S"  # acesta este hard coded pentru `xl2roefact` - a face mai mult de atit poate un ERP #TODO subject of documentation update
+        #FIXME.SYNTAX.ERR    tmp_InvoiceLine_list.append(tmpCompondedVAT: {req_item_info})  #FIXME original assigned only `req_item_info`. This version assign a "keyed" dict in order to deduplicate and sum (ie, do GROUP BY). #NOTE: keep like that it consider a good solution
+        '''  # ERROR: revert to prev line after error text
+        libutils.py", line 81
+        tmp_InvoiceLine_list.append(tmpCompondedVAT: {req_item_info})  #FIXME original assigned only `req_item_info`. This version assign a "keyed" dict in order to deduplicate and sum (ie, do GROUP BY). #NOTE: keep like that it consider a good solution      ^^^
+        SyntaxError: invalid syntax
+        '''  #FIXME next is reverted / previous line because error. drop when fixed
         tmp_InvoiceLine_list.append(req_item_info)
-    # ...??? build dict parts of final structure
+        
+    # ...??? build dict final dict structure (as commented at start of this function)
     ...
     return tmp_InvoiceLine_list
 
