7b69a307875d6385c8bfa3ca611ead8983398c5b
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-03-09 06:09:35 +0200

    Update __version__.py

--- xl2roefact/xl2roefact/__version__.py
+++ xl2roefact/xl2roefact/__version__.py
@@ -24,3 +24,14 @@
 __version__ = "0.3.1b1"
 
 
+
+'''TODO: work for generating a normalized / canonical version string
+
+pip install packaging
+
+packaging.utils.canonicalize_version(version)
+    # get a version string and return irs canonical form
+
+'''
+
+
