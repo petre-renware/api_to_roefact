a226fb2378f73de0e1dd853a42ab30e43d90396a
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-03-14 23:43:06 +0200

    wip... `0.4.0.dev2 item 4.`
    * Update app_cli.py
    
    * Update adhoc.cmd
    
    * Automated adhoc command run (by RENware Software Systems)
    
    * Update app_cli.py
    
    * Automated adhoc command run (by RENware Software Systems)
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/adhoc.cmd
+++ tests/adhoc.cmd
@@ -1,42 +1,20 @@
 
-rem ---------------
 rem copyright (c) 2024 Petre Iordanescu, petre.iordanescu@yahoo.com, RENware Software Systems
 rem this command script will be run on CI automation workflow:
 rem     - op sys: Windows
-rem     - workflow; `adhoc-run.yml`
 rem     - trigger event: merge to branch `adhoc`
 rem     - stdout redirection: `./tests/_test_results.txt`
 
 
 
 
-
-rem *** Change directory to XL2ROEFACT project component
 cd xl2roefact
 
-rem *** CREATE NEEDED ENVIRONMENT 
 python -m pip install pdm
 python -m pdm install
 
-rem *** BUILD WHEEL SCRIPT
-pdm info
-pdm run build_wheel
+pdm run xl2roefact about
 
-
-
-
-
-
-
-rem *** Change directory to INVOICETOROEFACT project component
-rem cd ..
-
-rem Create needed environment
-rem python -m pip install pdm
-rem python -m pdm install
-
-rem Effective script
-pdm info
 
 
 
--- xl2roefact/xl2roefact/app_cli.py
+++ xl2roefact/xl2roefact/app_cli.py
@@ -7,6 +7,9 @@ Identification:
 * author: Petre Iordanescu (petre.iordanescu@gmail.com)
 """
 
+# version objects
+from xl2roefact.__version__ import __doc__ as app_logo
+from xl2roefact.__version__ import normalized_version
 
 # general libraries
 import typer
@@ -21,8 +24,6 @@ from rich.pretty import pprint
 from rich.markdown import Markdown
 
 # xl2roefact specific libraries
-from xl2roefact import __version__ as appver
-from xl2roefact.__version__ import normalized_version
 import xl2roefact.config_settings as configs  # configuration elements to use with `settings` command
 from xl2roefact.rdinv import rdinv  # status #TODO: wip...
 from xl2roefact.wrxml import wrxml  # status #FIXME: not yet started
@@ -42,15 +43,12 @@ def about():
     """provide a short application description.
     """
     version_string = normalized_version()
-    app_logo = appver.__doc__
-    app_logo = Markdown(app_logo)
-    #print(app_logo)  #FIXME @230314 commented because raise err in ` 0.4.dev2` version ref encoding: `...    encoding defaults to sys.getdefaultencoding(). errors defaults to 'strict'.    ...`
+    print(Markdown(app_logo))
     print(f"xl2roefact {version_string} application by RENware Software Systems (c) 2023, 2024")
     # about details
     print("[yellow]extract & convert Excel invoice files to JSON, XML and upload info to [cyan]RO ANAF e-Fact[/] system")
     print("Support: [yellow]www.renware.eu, petre.iordanescu@gmail.com[/]")
     print("Product code: [yellow]0000-0095[/]")
-    #print("Copyright (c) 2023, 2024 RENware Software Systems.")
     print("Usage: for detailed help use [yellow]xl2roefact --help[/]")
 
 
