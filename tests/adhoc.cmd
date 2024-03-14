fefc1f12610fa0bae6b85109714f45e1865f2b2c
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-03-14 06:10:41 +0200

    Update adhoc.cmd

--- tests/adhoc.cmd
+++ tests/adhoc.cmd
@@ -1,20 +1,45 @@
 
 rem ---------------
 rem copyright (c) 2024 Petre Iordanescu, petre.iordanescu@yahoo.com, RENware Software Systems
-rem this command script will be run on CI automation workflow;
+rem this command script will be run on CI automation workflow:
+rem     - op sys: Windows
 rem     - workflow; `adhoc-run.yml`
 rem     - trigger event: merge to branch `adhoc`
 rem     - stdout redirection: `./tests/_test_results.txt`
-rem     - crt directory; `xl2roefact/`
-rem -------------------------
-
-rem echo "=== Test run of `adhoc.cmd` ======"
 
 
 
-rem wip.tests.... pdm run pypi_publish >_test_results.txt
 
-rem ...wip.tests ... pyinstaller 
-pdm run build_sexe
+
+rem *** Change directory to XL2ROEFACT project component
+cd xl2roefact
+
+rem Create needed environment
+python -m pip install pdm
+python -m pdm install
+
+rem Effective script
+pdm info
+
+
+
+rem *** Change directory to INVOICETOROEFACT project component
+cd ..
+
+rem Create needed environment
+python -m pip install pdm
+python -m pdm install
+
+rem Effective script
+pdm info
+
+
+
+
+
+
+
+rem *** to build standalone EXE
+rem pdm run build_sexe
 
 
