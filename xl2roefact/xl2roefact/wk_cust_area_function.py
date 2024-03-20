9b9a8029a7fcf061da5e6069e7ef2d7fe868f814
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-03-20 04:54:38 +0200

    Create wk_cust_area_function.py

---
+++ xl2roefact/xl2roefact/wk_cust_area_function.py
@@ -0,0 +1,17 @@
+# 
+# work area to clean and generalize a function to solve CUSTOMER INFO:
+# -1. to replace as separated function the existing code for customer info
+# -2. to generalize it to work for customer info, but a "CUSTOMER" op/cmd to be sent as parameter
+# -3. to be able to work for op/cmd "SUPPLIER"
+# -4. to be placed in `rdinv.py` module and to do work in new environment without disturbing it for existing functionalities
+#     ... ie, to not induce lateral effects
+#
+
+
+
+
+
+
+
+
+
