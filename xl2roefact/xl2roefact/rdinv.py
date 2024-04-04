9da8262e6657b44c4f09435cc0c67dc89d237028
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-04 11:31:13 +0300

    closed `owner-dbs-file + 6.`

--- xl2roefact/xl2roefact/rdinv.py
+++ xl2roefact/xl2roefact/rdinv.py
@@ -901,29 +901,28 @@ def get_partner_data(
                 "location": "external file",
             }
         }
-        # write data in corresponding keys
-        ... # CUI
+        # CUI
         param_invoice_header_area[unif_partner_area_key]["CUI"] = {
             "value": suppl_data_read["PartyLegalEntity"]["CompanyID"],
             "location": "external file (PartyLegalEntity -> CompanyID)",
             "label_value": None,
             "label_location": None
         }
-        ... # RegName
+        # RegName
         param_invoice_header_area[unif_partner_area_key]["RegistrationName"] = {
             "value": suppl_data_read["PartyLegalEntity"]["RegistrationName"],
             "location": "external file (PartyLegalEntity -> CompanyID)",
             "label_value": None,
             "label_location": None
         }
-        ... # PostalAddress
+        # PostalAddress
         param_invoice_header_area[unif_partner_area_key]["PostalAddress"] = {
             "cbc_StreetName": suppl_data_read["PostalAddress"]["StreetName"],
             "cbc_CityName": suppl_data_read["PostalAddress"]["CityName"],
             "cbc_PostalZone": suppl_data_read["PostalAddress"]["PostalZone"],
             "cac_Country": { "cbc_IdentificationCode": suppl_data_read["PostalAddress"]["CountryCode"] },
         }
-        ... # Tax & Contact
+        # Bank, Tax & Contact
         param_invoice_header_area[unif_partner_area_key]["reg_com"] = {
             "value": suppl_data_read["PartyTaxScheme"]["CompanyID"],
             "location": "external file (PartyLegalEntity -> CompanyID)"
@@ -936,18 +935,16 @@ def get_partner_data(
             "value": suppl_data_read["Contact"]["ElectronicMail"],
             "location": "external file (Contact -> ElectronicMail)"
         }
-        ... #TODO: ...wip first add them in YAML owner file
         param_invoice_header_area[unif_partner_area_key]["bank"] = {
-            "value": "...new field tbd... bank",
-            "location": "external file (...NOT YET DEFINED...)"
+            "value": suppl_data_read["Contact"]["Bank"],
+            "location": "external file (Contact -> Bank)"
         }
         param_invoice_header_area[unif_partner_area_key]["IBAN"] = {
-            "value": "...new field tbd... IBAN",
-            "location": "external file (...NOT YET DEFINED...)"
+            "value": suppl_data_read["Contact"]["IBAN"],
+            "location": "external file (Contact -> IBAN)"
         }
-        return  #TODO... then ret to line 255 and complete there
-    else:
-        # accept only known operations
+        return
+    else:  # accept only known operations
         raise Exception("partner_type parameter not recognized value")
     #
     # find invoice partner ==> one of (cac:AccountingCustomerParty , cac:AccountingSupplierParty)
