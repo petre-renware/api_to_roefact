38f0884b11b8b249dc5c8232841981a65d02811d
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-06 11:39:17 +0300

    Update __version__.py

--- xl2roefact/xl2roefact/__version__.py
+++ xl2roefact/xl2roefact/__version__.py
@@ -33,13 +33,16 @@ def normalized_version(
     """transform version string in canonical form.
 
     Usage:
+    
     - `import xl2roefact`
     - `xl2roefact.__version__.normalized_version()`
 
     Args:
+    
         `raw_version (str)`: a raw version string. Defaults to package current version string.
 
     Returns:
+    
         `str:` canonical version string
     """
     return packaging.utils.canonicalize_version(raw_version)
