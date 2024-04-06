96f306f5442e17c03ad43a51a439e3dccd224a5c
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-06 12:06:12 +0300

    Update libutils.py

--- xl2roefact/xl2roefact/libutils.py
+++ xl2roefact/xl2roefact/libutils.py
@@ -1,4 +1,4 @@
-"""general utilities library for all `xl2roefact` components and modules.
+7"""general utilities library for all `xl2roefact` components and modules.
 
 Identification:
 
@@ -77,8 +77,10 @@ def complete_sexe_file(
 ) -> bool:
     """Rename and move resulted exe file. This function is dedicated only to development phase, so various objects are hard coded.
 
-    * Specs: file to process `.../dist_sexe/xl2roefact_to_update_name.exe` --> `.../dist/xl2roefact-version-win64.exe
-    * NOTE: all function code suppose that current directory is root of `xl2roefact`, ie where is located `pyproject.toml` of package
+    Specs:
+
+    * `file to process `.../dist_sexe/xl2roefact_to_update_name.exe` --> `.../dist/xl2roefact-version-win64.exe
+    * Note 1: all function code suppose that current directory is root of `xl2roefact`, ie where is located `pyproject.toml` of package
 
     Args:
     
