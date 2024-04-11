3042e78e50c904cdb191672e16a348acd8564e1e
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-11 19:06:32 +0300

    closed `cac_Delivery`

--- xl2roefact/xl2roefact/rdinv.py
+++ xl2roefact/xl2roefact/rdinv.py
@@ -334,6 +334,9 @@ def rdinv(
             "cac_PaymentMeans": {
                 "cbc_PaymentMeansCode": 1  #NOTE ? do not know if simple Excel processing can give this info - NEEED ERP
             },
+            "cac_Delivery": {
+                "cbc_ActualDeliveryDate": copy.deepcopy(invoice_header_area["issued_date"]["value"])  # suppose identical with invoice date. Format: `YYYY-MM-DD`
+            },
             # TODO: ... ... ...
 
 
