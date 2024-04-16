372b6e513dec22c4eda6c3f556c6f79a0fd50419
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-16 15:08:04 +0300

    lin 259 Update rdinv.py

--- xl2roefact/xl2roefact/rdinv.py
+++ xl2roefact/xl2roefact/rdinv.py
@@ -250,9 +250,13 @@ def rdinv(
         # apply `DEFAULT_DUE_DATE_DAYS` to invoice issue date
         #NOTE use `a date + timedelta(days = 2)`
         #FIXME nxt line is test tb replaced
-        due_date_info["value"] = DEFAULT_DUE_DATE_DAYS  #FIXME test tb replaced
+        due_date_info["value"] = f"wip... {DEFAULT_DUE_DATE_DAYS=}"  #FIXME test tb replaced
         # convert invoice issued date to date ... hint line 254
-        # code... invoice_issued_date_as_date = ...
+        invoice_issued_date_as_date = datetime.strptime(
+            invoice_header_area["issued_date"]["value"],
+            '%Y-%m-%d'
+        )
+        print(f"****AVEM ASA: {invoice_issued_date_as_date=}")  #FIXME dbg can drop
         # code... due_date_info["value"] = invoice_issued_date_as_date + timedelta(days = DEFAULT_DUE_DATE_DAYS)`
     invoice_header_area["due_date"] = copy.deepcopy(due_date_info)
     # ...
