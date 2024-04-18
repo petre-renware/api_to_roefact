f283f44085aa8be64633c35ba6e89dac863bda00
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-18 06:48:55 +0300

    Update sys_settings.py

--- xl2roefact/xl2roefact/sys_settings.py
+++ xl2roefact/xl2roefact/sys_settings.py
@@ -1,1 +1,21 @@
+"""Configuration and setting parameters.
 
+Regulile recomandate se gasessc in documentul (recommended rules are in document `xl2roefact/data/README_app_config_rules.md`)
+
+Public objects:
+
+* `rules_content`: contains the rules text (rendered)
+
+Info:
+
+* copyright: (c) 2023 RENWare Software Systems
+* author: Petre Iordanescu (petre.iordanescu@gmail.com)
+"""
+
+from pathlib import Path
+import os
+import sys
+from rich.markdown import Markdown
+import yaml
+from pprint import pprint
+from .libutils import hier_get_data_file
