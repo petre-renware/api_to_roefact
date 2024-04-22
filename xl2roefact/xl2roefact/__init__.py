f5e1e5347788950887e4144b44f7288658f1c4be
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-04-22 07:57:51 +0300

    Update __init__.py

--- xl2roefact/xl2roefact/__init__.py
+++ xl2roefact/xl2roefact/__init__.py
@@ -7,7 +7,6 @@ from xl2roefact.wrxml import wrxml  # status #FIXME: not yet started
 from xl2roefact.chkxml import chkxml  # status #FIXME: not yet started
 from xl2roefact.ldxml import ldxml  # status #FIXME: not yet started
 from xl2roefact.chkisld import chkisld  # status #FIXME: not yet started
-import xl2roefact.sys_settings
 
 __version__ = normalized_version()  # default conversion takes place over xl2roefact actual version
 
