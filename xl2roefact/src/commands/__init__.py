d858451ca3a34a91b0a6bdb5c2655d6196065786
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-05-02 05:42:31 +0300

    Update __init__.py

--- xl2roefact/src/commands/__init__.py
+++ xl2roefact/src/commands/__init__.py
@@ -1,4 +1,4 @@
-"""xl2roefact in-package designed to implement commands layer
+"""xl2roefact in-package designed to implement *commands layer*
 
 * Issued: 2024 May
 * Author: Petre Iordanescu, petre.iordanescu@gmail.com
