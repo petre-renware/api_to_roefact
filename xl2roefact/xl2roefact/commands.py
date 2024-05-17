a0a3fa814128bda072b383ee62318e7cc191b756
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-05-17 07:49:41 +0300

    Update commands.py

--- xl2roefact/xl2roefact/commands.py
+++ xl2roefact/xl2roefact/commands.py
@@ -1,6 +1,13 @@
 """Layer 2 commands implementation.
 
+Objectives:
 
+* create an environment wheree a xl2roefact can be run in *session or interactivelly mode*
+* session parameters: persist commands run parameters in user profile (directory of `os.%userprofile%` or Linux `~/.profile`)
+* group all layer 2 commands for:
+    * `xl2roefactd` (aka server)
+    * `xl2roefact-client` (aka console client)
+    * `web2roefact` (aka web client UI front end)* components
 
 
 Identification:
