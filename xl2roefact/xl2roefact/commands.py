d84910c0a7f0ddd2b1ba10b2ec1da6034f716ab7
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-05-18 16:29:04 +0300

    Update commands.py

--- xl2roefact/xl2roefact/commands.py
+++ xl2roefact/xl2roefact/commands.py
@@ -18,9 +18,9 @@ Identification:
 
 #TODO get all imports from app_clu. see what keep here...
 # version objects
-from . import __version__
-#from .__version__ import __doc__ as app_logo
-#from .__version__ import normalized_version as normalized_version
+#from . import __version__
+from .__version__ import __doc__ as app_logo
+from .__version__ import normalized_version as normalized_version
 
 # general libraries
 import typer
