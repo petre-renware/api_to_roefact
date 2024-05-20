a78b0e8d061abf5003185f6379ab5805cfc422d3
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-05-20 18:28:44 +0300

    Update commands.py

--- xl2roefact/xl2roefact/commands.py
+++ xl2roefact/xl2roefact/commands.py
@@ -57,9 +57,7 @@ class SessionDataType:
 
 
 class Commands:
-    """xl2roefact commands layer implementation.
-
-    More [details here](../doc/README_xl2roefact_library.md)
+    """xl2roefact commands layer implementation. [Details here](../doc/README_xl2roefact_library.md)
     """
 
     # Default session data values (used when no other data is provided). Default values are only for "clear known" variables
