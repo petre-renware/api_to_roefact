3840faa631233b9f5c45018eaba95512017f2316
Petre Iordanescu <petre.iordanescu@gmail.com>
2024-05-22 07:43:03 +0300

    wip... `0.9+main-code` importes xl2json code

--- xl2roefact/xl2roefact/commands.py
+++ xl2roefact/xl2roefact/commands.py
@@ -133,17 +133,72 @@ class Commands:
 
     def xl2json(
         self,
-        tbd  #TODO: rest of params
-    ) -> ...:
+        invoice_type: InvoiceTypesEnum  = "...",
+        file_name: str = "...",
+        files_directory: Path = "...",
+        owner_datafile: Path = "...",
+        verbose: bool = "..."
+    ) -> ...:  #FIXME what returns? at least: console messages & execution state (ie, did or not something)
         """read excel invoice and generate a JSON file with invoice data, miscellaneous meta and original Excel found data
 
         Args:
-            `...`: ...
+            `invoice_type_code`: invoice type (for example regular invoice or storno) as this info is not usually subject of Excel file. Default to `380` (regular / usual invoice)
+            `file_name`: files to process (wildcards allowed).
+            `files_directory`: directory to be used to look for Excel files. Defaults to `invoice_files/`. NOTE: if default directory does not exists will consider current directory instead
+            `owner_datafile`: File to read invoice supplier (owner) data instead Excel.
+            `verbose`: show detailed processing messages". Defaults to `False`.
 
         Return:
             `...`: ...
         """
-        ...  # TODO: code.me
+        # TODO; for not specified parameters get default values from session data...
+        if invoice_type == "...":  # TODO: repeat for all params...
+            ...
+        #FIXME: imported code starts here
+        console = Console() #TODO: redirect out to a file a variable to collect and return it at finish...
+        console.print(f"*** Application [red]xl2roefact[/] launched at {datetime.now()}")
+        # prep Excel files to rocess as requested in command line (NOTE: if default directory does not exists will consider current directory instead)
+        tmp_files_to_process = Path(files_directory)
+        if not (tmp_files_to_process.exists() and tmp_files_to_process.is_dir()):
+            tmp_files_to_process = Path(".").absolute()
+            console.print(f"[dark_orange]WARNING note:[/] Default directory not found. Will consider current directory: [cyan]{tmp_files_to_process}[/].")
+        if verbose:
+            console.print(f"[yellow]INFO note:[/] files to process: [cyan]{Path(tmp_files_to_process, file_name)}[/]")
+            console.print()
+        list_of_files_to_process = list(tmp_files_to_process.glob(file_name))  # `glob()` will unify in a list with specified files as pattern
+        # process Excel files list
+        for a_file in list_of_files_to_process:
+            rdinv_run_messages = list()  # this will collect rdinv running messages and if verbose is True will print
+            invoice_to_process = Path("./", a_file)  # current file name to process, starting from current directory (the `files_directory` is already contained in)
+            # prep for owner data acquiring from external data-file or from Excel
+            if owner_datafile is not None:  # prep are to call `rdinv()` module with parameter to read supplier data from external file instead Excel
+                full_path_owner_datafile = hier_get_data_file(owner_datafile)
+                if full_path_owner_datafile:
+                    invoice_datadict = rdinv(
+                        invoice_type_code=invoice_type,
+                        file_to_process=invoice_to_process,
+                        debug_info=rdinv_run_messages,
+                        owner_datafile=full_path_owner_datafile
+                    )
+                    if verbose:
+                        for msg in rdinv_run_messages:
+                            console.print(msg)
+                else:
+                    console.print(f"[red]ERROR: Owner data file ([cyan]{owner_datafile}[/]) is not valid or does not exists. Process terminated.[/].")
+                    return  # just exit...
+            else:
+                invoice_datadict = rdinv(
+                    invoice_type_code=invoice_type,
+                    file_to_process=invoice_to_process,
+                    debug_info=rdinv_run_messages
+                )
+                if verbose:
+                    for msg in rdinv_run_messages:
+                        console.print(msg)
+            if not invoice_datadict:
+                console.print(f"[yellow]INFO note:[/] last step returned an empty invoice JSON and process could be incomplete. Please review previous messages.")
+        #FIXME: imported code ends here
+        
 
 
     def settings(
